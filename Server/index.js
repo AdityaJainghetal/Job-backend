264e6fb2ff519ea6ac6a2cde4a1d6d548e30d6ad
Aditya Jain <adityajainghetal@gmail.com>
2025-07-01 16:20:52 +0530

    task

--- Server/index.js
+++ Server/index.js
@@ -1,28 +1,29 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
-const path = require('path');
-const dbconnect = require('./Utils/db.config')
-const cors = require('cors');
-const morgan = require('morgan');
+const path = require("path");
+const dbconnect = require("./Utils/db.config");
+const cors = require("cors");
+const morgan = require("morgan");
 
-
-
-
-
-
-require("dotenv").config()
-const mongoose= require("mongoose");
-const bodyparser = require('body-parser')
-const fileUpload = require('express-fileupload');
-
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
 app.use(morgan("tiny"));
 
-require('dotenv').config();
-app.use(cors());
+require("dotenv").config();
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials:true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 // app.use(cors());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 dbconnect();
 app.use(fileUpload());
@@ -32,49 +33,29 @@ const UserEmployeelogin = require("./Route/EmployeeLoginRoute");
 const CategoryRoute = require("./Route/JobRoute/CategoryRoute");
 const SubCategoryRoute = require("./Route/JobRoute/subsubRoute");
 const jobRoute = require("./Route/JobRoute/JobRoute");
-const jobapply = require("./Route/JobapplyRoute/JobapplyRoute")
+const jobapply = require("./Route/JobapplyRoute/JobapplyRoute");
 
 // app.use("/uploads", express.static("uploads"));
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-let PORT = process.env.PORT || 8000
-
+let PORT = process.env.PORT || 8000;
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Body-parser middleware
-app.use(bodyparser.urlencoded({ extended: true }))
-app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 app.use("/user", UserEmployee);
-app.use("/employee",UserEmployeelogin);
-app.use("/category", CategoryRoute)
+app.use("/employee", UserEmployeelogin);
+app.use("/category", CategoryRoute);
 app.use("/subcategory", SubCategoryRoute);
-app.use("/api",jobRoute);
-app.use("/jobapply", jobapply)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/api", jobRoute);
+app.use("/jobapply", jobapply);
 
 app.listen(PORT, function (error) {
-    if (error) throw error
-    console.log("Server created Successfully on PORT: ", PORT)
-})
+  if (error) throw error;
+  console.log("Server created Successfully on PORT: ", PORT);
+});
